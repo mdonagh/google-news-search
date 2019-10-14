a07e731b323788c46218f9a08adb69c6662e3acf
Mark Donahue <mdonagh@icloud.com>
2019-10-13 19:17:26 -0700

    secret key

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,7 @@ set :application,     'google-news-search'
 set :user,            'deploy'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
+set :keep_releases, 1
 
 # Don't change these unless you know what you're doing
 set :pty,             true
