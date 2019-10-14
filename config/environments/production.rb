c3e98b804232fd3c3543b106471c13021f5f23c0
Mark Donahue <mdonagh@icloud.com>
2019-10-13 18:52:35 -0700

    secret key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,6 +53,8 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+config.require_master_key = true
+
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
